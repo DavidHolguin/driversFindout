767f62c1b39dfc89561d34fbe0953a5743fe3acd
CHdev <codigo.turismointeractivo360@gmail.com>
2024-11-10 21:45:23 -0500

    Primer commit drivers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import CompanyList from './components/CompanyList';
-import CompanyDetail from './components/CompanyDetail';
-import Search from './components/Search';
+
 import Login from './components/Login';
 import Register from './components/Register';
 import MenuBar from './components/MenuBar';
 import Profile from './components/Profile';
 import Settings from './components/Settings';
-import CompanyCategory from './components/CompanyCategory';
-import JoinFindoutPage from './components/JoinFindoutPage';
-import QRScanner from './components/QRScanner';
-import ShoppingCartComponent from './components/ShoppingCartComponent';
+
 import IOSInstallGuide from './components/IOSInstallGuide'; // Add this import
 
 function App() {
@@ -45,19 +40,18 @@ function App() {
         <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
         <main className="container mx-auto pt-16 pb-14 text-gray-900 dark:text-white">
           <Routes>
-            <Route path="/" element={<CompanyList />} />
-            <Route path="/company/:id" element={<CompanyDetail />} />
-            <Route path="/search" element={<Search />} />
+            <Route path="/" element={<Settings />} />
+
+   
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/scanQR" element={<QRScanner />} />
+
             <Route path="/settings" element={<Settings />} />
-            <Route path="/company-categories/:categoryId" element={<CompanyCategory />} />
-            <Route path="/register-business" element={<JoinFindoutPage />} />
+
             <Route path="/download" element={<IOSInstallGuide />} /> 
             
-            <Route path="/cart/:companyId" element={<ShoppingCartComponent />} />     
+           
           </Routes>
         </main>
         <MenuBar />
